374d1c5b01851c9167b54b2de5fc2136ce339826
Daaypeite <daiane.peite@hotmail.com>
2022-07-28 13:28:43 -0300

    first commit

---
+++ src/ExemploLaboratorio1/Laboratorio1.java
@@ -0,0 +1,15 @@
+package ExemploLaboratorio1;
+
+public class Laboratorio1 {
+
+	public static void main(String[] args) {
+		int valor1, valor2,resultado;
+		valor1 = 10;
+		valor2 = 20;
+		resultado = valor1 + valor2;
+		System.out.println("10 + 20 = " + resultado);
+		
+
+	}
+
+}
---
+++ src/ExemploLaboratorio1/Laboratorio2.java
@@ -0,0 +1,11 @@
+package ExemploLaboratorio1;
+
+public class Laboratorio2 {
+
+	public static void main(String[] args) {
+		int valor = 10;
+		System.out.println((valor % 2 ==0)? "Par" : "impar" );
+
+	}
+
+}
